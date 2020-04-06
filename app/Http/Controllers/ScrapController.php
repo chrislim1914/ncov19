9ee5d7408e7c7d721374f149e277825ffc549545
chris lim <lm.chrstphr.m@gmail.com>
2020-04-06 10:24:36 +0800

    another update of column in https://www.worldometers.info/coronavirus/

--- app/Http/Controllers/ScrapController.php
+++ app/Http/Controllers/ScrapController.php
@@ -24,7 +24,7 @@ class ScrapController extends Controller
                     'casesPerOneMillion'    => $wmdata[$counter + 8],
                     'deathPerOneMillion'    => $wmdata[$counter + 9],
                 );    
-                $counter += 10;
+                $counter += 12;
             } while(count($wmdata) > $counter);
 
             return response()->json([
@@ -59,7 +59,7 @@ class ScrapController extends Controller
                         'deathPerOneMillion'    => $wmdata[$counter + 9],
                     );  
                 }  
-                $counter += 10;
+                $counter += 12;
             } while(count($wmdata) > $counter);
 
             return response()->json([
@@ -111,7 +111,7 @@ class ScrapController extends Controller
                     }
                     array_push($countryholder, $wmdata[$counter + 0]);
                 }                
-                $counter += 10;
+                $counter += 12;
             } while(count($wmdata) > $counter);
 
             return $countryholder;
